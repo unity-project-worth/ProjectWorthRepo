d38cb5a8487e3ea44428f8dcb30ac02580d98b86
Keaton Freude <freudek90@gmail.com>
2013-08-20 21:48:47 -0700

    Network Exploration
    Added Create/Join server buttons. Allows each connected player to
    control their own sprite that is synced between clients.

--- ProjectWorth/Assets/MovementScript.cs
+++ ProjectWorth/Assets/MovementScript.cs
@@ -5,6 +5,11 @@ public class MovementScript : MonoBehaviour {
 	public float move_speed = 5.0f;
 	public float jump_height = 10;
 	tk2dSprite sprite;
+	
+	float lastNetworkTime = 0f;
+	
+	Vector3 LastPosition = Vector3.zero;
+	Vector3 NextPosition = Vector3.zero;
 	// Use this for initialization
 	void Start () {
 		sprite = GetComponent<tk2dSprite>();
@@ -12,31 +17,38 @@ public class MovementScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.W))
-		{
-			//Jump!
-			rigidbody.velocity += Vector3.up * jump_height;
-			Debug.Log ("Jump!");
-		}
-		if (Input.GetKey(KeyCode.D))
-		{
-			//Move Right
-			this.transform.localPosition += new Vector3(move_speed * Time.deltaTime, 0, 0);
-			sprite.FlipX = false;
-		}
-		
-		if (Input.GetKey (KeyCode.A))
-		{
-			sprite.FlipX = true;
-			this.transform.localPosition += new Vector3(-move_speed * Time.deltaTime, 0, 0);
-		}
-		
 
-
+		
 	}
 	
 	void FixedUpdate()
 	{
 		
 	}
+	
+	/*void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Debug.Log ("HELLO!");
+		if (stream.isWriting)
+		{
+			Vector3 pos = this.transform.localPosition;
+			Quaternion rot = this.transform.rotation;
+			stream.Serialize(ref pos);
+			stream.Serialize(ref rot);
+		}
+		else
+		{
+			Vector3 pos = Vector3.zero;
+			Quaternion rot = Quaternion.identity;
+			
+			stream.Serialize(ref pos);
+			stream.Serialize(ref rot);
+			
+			
+			LastPosition = this.transform.position;
+			NextPosition = pos;
+			
+			//lastNetworkTime;
+		}
+	}*/
 }
---
+++ ProjectWorth/Assets/NetworkingTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkingTest : MonoBehaviour {
+	public GameObject ToInstantiate;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect(0, Screen.height *.2f, 100, 25), "Create Server"))
+		{
+			Network.InitializeServer(10, 8000, true);
+			Network.Instantiate(ToInstantiate, new Vector3(26, 20, 0), Quaternion.identity, 0);
+		}
+		
+		if (GUI.Button (new Rect(0, Screen.height * .25f, 100, 25), "Join Server"))
+		{
+			Network.Connect("127.0.0.1", 8000);
+			
+		}
+	}
+	
+	void OnConnectedToServer()
+	{
+		Network.Instantiate(ToInstantiate, new Vector3(26, 20, 0), Quaternion.identity, 0);
+	}
+}
